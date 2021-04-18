cf171bb0d9feafbbd9b11aa3a0f411ea2f12e7a4
gabriele <gabriele.cerqueglini@gmail.com>
2021-04-18 21:31:17 +0200

    header scroll and anchor ok

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
-//var
-const headerElem=document.querySelector('header');
 
-//functions
+const headerElem=document.querySelector('header');
 
 /**
  * HELPER FUNCTIONS
@@ -10,23 +8,36 @@ function readSections(){
     return document.querySelectorAll('section[id^=sect-]'); 
 }
 
-/* check if elem is full visible or not*/
-function elemIsFullVisible(elem){
-    let elemRect=elem.getBoundingClientRect();
+function pageScrolled(){
+    console.log(window.pageYOffset);
+    if (window.pageYOffset > 0){
+        headerElem.classList.add('page-scrolled');
+        if (window.pageYOffset + window.innerHeight >= document.body.offsetHeight){
+            document.getElementById('bot-anchor').setAttribute('style','display:inline;')
+        }
+        else {
+            document.getElementById('bot-anchor').removeAttribute('style','display:inline;')
+        }
+    }
+    else{
+        headerElem.classList.remove('page-scrolled');
+    }
+}
 
-    //check if elem is full visible
-    if (elemRect.top >= 0 && (elemRect.bottom - window.innerHeight)<=0){
+// Check if elem is visible > 50% of its height
+function elemIsVisible(elem){
+    //distance from the top of elem and the bottom of viewport
+    const topToWbot=window.innerHeight - elem.getBoundingClientRect().top;
+    const elemH=elem.offsetHeight;
+
+    //return true if element is visible from the upper half to the low half
+    if (topToWbot >= elemH*0.5 && topToWbot <= elemH*1.5){
         return true;
     }
     else{
         return false;
     }  
 }
-
-function getElemPos(elem){
-    
-}
-
 /**
  * END HELPER FUNCTIONS
 */
@@ -34,8 +45,7 @@ function getElemPos(elem){
 /**
  * MAIN FUNCTIONS
 */
-
-/*func for populating nav menu*/
+// Populating nav menu
 function createMenuList(){
     const sections=readSections();
     //creating a fragment to append new <li> elements
@@ -57,19 +67,31 @@ function createMenuList(){
     document.querySelector('.menu-header-list').appendChild(fragment);
 }
 
-// Add class 'active' to section when near top of viewport
+// Add class active-element to section when at least 50% visible on the viewport
 function sectionActiveClass(){
-    
+        let sections=readSections();
+        for (section of sections){
+            if (elemIsVisible(section)){
+                section.classList.add('active-element');
+                //add class active to the correspondent anchor
+                document.querySelector('.menu-header-list a[href=\'#'+section.id+'\']').classList.add('active-anchor');
+            }
+            else {
+                section.classList.remove('active-element');
+                //remove class active from the correspondent anchor
+                document.querySelector('.menu-header-list a[href=\'#'+section.id+'\']').classList.remove('active-anchor');
+            }
+        }
+    }
+
+// Scroll to anchor
+function scrollToAnchor(e){
+    e.preventDefault();
+    const anchorSec=document.querySelector(e.target.hash);
+    const posY=anchorSec.getBoundingClientRect().top + document.documentElement.scrollTop - headerElem.offsetHeight;
+
+    window.scrollTo({left: 0, top: posY, behavior: 'smooth'});
 }
-
-// Scroll to anchor ID using scrollTO event
-function scrollTo(){
-    window.scrollTo(x,(y-headerElem));
-    Element.offsetTop; //distance from top
-    Element.offsetParent;
-}
-
-
 /**
  * END MAIN FUNCTIONS
 */
@@ -77,21 +99,17 @@ function scrollTo(){
 /**
  * EVENTS 
 */
-
-//creating list inside ul
+// Creating list inside ul
 createMenuList();
 
 // Scroll to section on link click
+document.querySelector('.menu-header-list').addEventListener('click',scrollToAnchor);
 
-// Set sections as active
-document.addEventListener('scroll',function(){
-    let sections=readSections();
-    for (section of sections){
-        if (elemIsFullVisible(section)){
-            section.classList.add('active-element');
-        }
-        else{
-            section.classList.remove('active-element');
-        }
-    }
+// Scroll to top on bottom link click
+document.querySelector('#bot-anchor').addEventListener('click', e => {
+    e.preventDefault();
+    window.scrollTo({left: 0, top: document.querySelector('#page-top').offsetTop, behavior: 'smooth'});
 });
+
+// EventListener for active section
+document.addEventListener('scroll', e => {pageScrolled(); sectionActiveClass(e)});
